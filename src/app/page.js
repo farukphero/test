f181f5721c776660d1f51e9a6b89097b7b505649
farukphero <farukk6319@gmail.com>
2024-03-08 20:11:16 +0600

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
-import Image from "next/image";
-
+import { app } from "@/firebase/firebase.config";
+import { getAuth} from "firebase/auth";
 export default function Home() {
+  const auth = getAuth(app);
+  console.log(auth)
   return (
     <div>
       Hello world
